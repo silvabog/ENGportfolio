9c1f930025ec24346ffcd98a5ff1687c6d3ef243
Gabriel Silva <silvabog@kean.edu>
2024-10-26 01:35:39 -0400

    commit

--- src/index.js
+++ src/index.js
@@ -8,3 +8,14 @@ root.render(
   </React.StrictMode>
 );
 
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(9000);
